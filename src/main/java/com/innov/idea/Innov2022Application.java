21a40c3de4eee57e7fda2bfc692c05dcf65c907f
Manandhar, Saurav <saurav.manandhar@optum.com>
2022-12-06 21:51:58 -0600

    initial commit

---
+++ src/main/java/com/innov/idea/Innov2022Application.java
@@ -0,0 +1,13 @@
+package com.innov.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Innov2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Innov2022Application.class, args);
+	}
+
+}
